fe03bb194362e81b2e5e70ca719484c99d86e7d8
theonekingboo <Juankingboo@proton.me>
2024-04-25 16:52:10 -0400

    improved UI
    -Impoved UI for buttons
    -Added template for legal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import Copyright from './components/Copyright';
 
 function App() {
-
   return (
     <>
       <div>
         <h1 className='text-4xl font-bold underline'>Buz50</h1>
         <p className='font-bold'>building in progress</p>
+        <button className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4'>Join Game</button>
       </div>
+      <div className="mt-4"> 
+        <button className='bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Create Game</button>
+      </div>
+      <Copyright />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Copyright.css';
+
+function Copyright() {
+  return (
+    <div className="copyright-container">
+      <p>Buz50 version 0.1, Copyright &copy; 2024 Juan Rojas</p>
+    <button>Buz50 comes with ABSOLUTELY NO WARRANTY; for details click on this text.</button>
+    <button>This is free software, and you are welcome to redistribute it under certain conditions; click on this text for details.</button>
+    </div>
+  );
+}
+
+export default Copyright;
